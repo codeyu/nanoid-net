1a41b2d69806788390a1dd10e939a89f23a73f29
codeyu <codeyu@gmail.com>
2017-12-24 19:48:46 +0800

    init

---
+++ src/Nanoid/Nanoid.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace Nanoid
+{
+    public static class Nanoid
+    {
+        private static readonly Random random = new Random();
+        public static String Generate(string alphabet="_~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", int size=21)
+        {
+            return Generate(random, alphabet, size);
+        }
+        public static String Generate(Random random, string alphabet="_~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", int size=21)
+        {
+
+            if (random == null)
+            {
+                throw new ArgumentNullException("random cannot be null.");
+            }
+
+            if (alphabet == null)
+            {
+                throw new ArgumentNullException("alphabet cannot be null.");
+            }
+
+            if (alphabet.Length == 0 || alphabet.Length >= 256)
+            {
+                throw new ArgumentOutOfRangeException("alphabet must contain between 1 and 255 symbols.");
+            }
+
+            if (size <= 0)
+            {
+                throw new ArgumentOutOfRangeException("size must be greater than zero.");
+            }
+
+            int mask = (2 << (int)Math.Floor(Math.Log(alphabet.Length - 1) / Math.Log(2))) - 1;
+            int step = (int)Math.Ceiling(1.6 * mask * size / alphabet.Length);
+
+            StringBuilder idBuilder = new StringBuilder();
+
+            while (true)
+            {
+
+                byte[] bytes = new byte[step];
+                random.NextBytes(bytes);
+
+                for (int i = 0; i < step; i++)
+                {
+
+                    int alphabetIndex = bytes[i] & mask;
+
+                    if (alphabetIndex < alphabet.Length)
+                    {
+                        idBuilder.Append(alphabet[alphabetIndex]);
+                        if (idBuilder.Length == size)
+                        {
+                            return idBuilder.ToString();
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ test/Nanoid.Test/NanoidTest.cs
@@ -0,0 +1,43 @@
+using System;
+using Xunit;
+using Nanoid;
+namespace Nanoid.Test
+{
+    public class NanoidTest
+    {
+        [Fact]
+        public void TestDefault()
+        {
+            var result = Nanoid.Generate();
+            Assert.Equal(21, result.Length);
+        }
+
+        [Fact]
+        public void TestCustomSize()
+        {
+            var result = Nanoid.Generate(size:10);
+            Assert.Equal(10, result.Length);
+        }
+        [Fact]
+        public void TestCustomAlphabet()
+        {
+            var result = Nanoid.Generate("1234abcd");
+            Assert.Equal(21, result.Length);
+        }
+
+        [Fact]
+        public void TestCustomAlphabetAndSize()
+        {
+            var result = Nanoid.Generate("1234abcd", 7);
+            Assert.Equal(7, result.Length);
+        }
+
+        [Fact]
+        public void TestCustomRandom()
+        {
+            var random = new Random(10);
+            var result = Nanoid.Generate(random);
+            Assert.Equal(21, result.Length);
+        }
+    }
+}
